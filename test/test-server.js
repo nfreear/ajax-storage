3a5477300ba1a4e44ecddd9a2ce10e27322222b2
Nick Freear <nfreear@yahoo.co.uk>
2014-10-09 22:12:27 +0100

    Bug #1, Fixes to "test-server" Javascript, package JSON, travis YAML

--- test/test-server.js
+++ test/test-server.js
@@ -1,13 +1,22 @@
-// Test HTTP server - Express.
-// http://ericsowell.com/blog/2014/6/17/enough-node-for-building-a-simple-website
+/*!
+  Test HTTP server - Express.
+  http://ericsowell.com/blog/2014/6/17/enough-node-for-building-a-simple-website
+*/
 
-var port = 8010;
+'use strict';
+
+var argv = require('minimist')(process.argv.slice(2));
+//console.dir(argv);
+
+var port = argv.port > 799 && argv.port < 9999 ? argv.port : 8010;
+var doc_root = __dirname + '/..';
+
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/..'));  // + '/public'
+app.use(express.static(doc_root));
 
-console.log('Dir: ' + __dirname);
+console.log('Doc root: ' + doc_root);
 
 // CORS middleware
 // http://stackoverflow.com/questions/7067966/how-to-allow-cors-in-express-nodejs
@@ -17,12 +26,21 @@ var allowCrossDomain = function (req, res, next) {
     res.header('Access-Control-Allow-Headers', 'Content-Type');
 
     next();
-}
+};
 
 //app.use(allowCrossDomain);
-
 //console.log('CORS ok');
 
 app.listen(port, function () {
     console.log('HTTP server listening on... http://127.0.0.1:' + port);
+});
+
+process.on('uncaughtException', function (ex) {
+    console.log('Error: ' + ex.message);
+    if ('EADDRINUSE' === ex.code) {
+        console.log(
+        '> Perhaps kill a forked Web server on port %s?'.replace('%s', port));
+        console.log('> # ps aux | grep node # kill PID');
+    }
+    process.exit(1);
 });
